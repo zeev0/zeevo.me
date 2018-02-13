af6a41ab4c42341a0e98c2911c98d1e2102f8bfe
zeevo <zeevozeevo@gmail.com>
2018-02-12 21:28:25 -0500

    Deployment scripts

---
+++ deploy/initiate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+### Configuration ###
+
+USER=ubuntu
+SERVER=$USER@ec2-13-59-198-83.us-east-2.compute.amazonaws.com
+REMOTE_SCRIPT_PATH=/tmp/deploy.sh
+APP_DIR=~/zeevo.me
+GIT_URL=~/zeevo.me.git
+IDENTITY_FILE=zeevome.pem
+
+### Library ###
+
+function run()
+{
+  echo "Running: $@"
+  "$@"
+}
+
+
+### Automation steps ###
+
+run scp -i deploy/$IDENTITY_FILE deploy/work.sh $SERVER:$REMOTE_SCRIPT_PATH
+echo
+echo "---- Running deployment script on remote server ----"
+run ssh -i deploy/$IDENTITY_FILE $SERVER bash $REMOTE_SCRIPT_PATH
---
+++ deploy/work.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+### Configuration ###
+
+APP_DIR=~/zeevo.me
+GIT_URL=~/zeevo/zeevo.me.git
+USER=$1
+
+###
+
+set -x
+
+# Pull latest code
+if [[ -e $APP_DIR ]]; then
+  cd $APP_DIR
+  git pull
+else
+  git clone $GIT_URL $APP_DIR
+  cd $APP_DIR/
+fi
+
+# Install dependencies
+npm install --production
+npm prune --production
+
+if tmux info &> /dev/null; then 
+  tmux kill-session -t "zeevome"
+fi
+
+tmux new-session -d -s "zeevome" "sudo npm run prod"
